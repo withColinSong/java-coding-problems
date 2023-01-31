09c6dc5ef525b07d202cfb2b07e3534847016d1b
withColinSong <withcolinsong@gmail.com>
2023-01-31 23:44:23 +0900

    add: vowels, consonants stream way

--- src/chapter01/Chapter005.java
+++ src/chapter01/Chapter005.java
@@ -1,6 +1,7 @@
 package chapter01;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @WIRTE ellie.song
@@ -17,6 +18,10 @@ public class Chapter005 {
         T1 right;
 
         public static Pair<Integer, Integer> of(int x, int y) {
+            return new Pair<>(x, y);
+        }
+
+        public static Pair<Long, Long> of(long x, long y) {
             return new Pair<>(x, y);
         }
 
@@ -46,12 +51,53 @@ public class Chapter005 {
         return Pair.of(vowels, consonants);
     }
 
+    // stream
+    public static Pair<Long, Long> countVowelsAndConsonants2(String str) {
+
+        String s = str.toLowerCase();
+
+        long vowels = str.chars()
+                        .filter(c-> allVowels.contains((char)c))
+                        .count();
+
+        long consonants = str.chars()
+                            .filter(c -> !allVowels.contains((char)c))
+                            .filter(c -> (c >= 'a' && c <= 'z'))
+                            .count();
+
+        return Pair.of(vowels, consonants);
+    }
+
+    // stream2
+    public static Pair<Long, Long> countVowelsAndConsonants3(String str) {
+
+        String s = str.toLowerCase();
+
+        Map<Boolean, Long> result = str.chars().mapToObj(c-> (char) c)
+                .filter(ch -> (ch >= 'a' && ch <= 'z'))
+                .collect(Collectors.partitioningBy(c-> allVowels.contains(c), Collectors.counting()));
+
+        return Pair.of(result.get(true), result.get(false));
+    }
+
+
     public static void main(String[] args) {
 
         String str = "ellie.song";
+
         Pair<Integer, Integer> integerIntegerPair = countVowelsAndConsonants(str);
+        Pair<Long, Long> integerIntegerPair2 = countVowelsAndConsonants2(str);
+        Pair<Long, Long> integerIntegerPair3 = countVowelsAndConsonants2(str);
+
+
         System.out.println(integerIntegerPair.left);
         System.out.println(integerIntegerPair.right);
+
+        System.out.println(integerIntegerPair2.left);
+        System.out.println(integerIntegerPair2.right);
+
+        System.out.println(integerIntegerPair3.left);
+        System.out.println(integerIntegerPair3.right);
 
     }
 
