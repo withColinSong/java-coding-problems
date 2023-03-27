d0492865d96a4525671e7a6cafe03824006c843a
withColinSong <withcolinsong@gmail.com>
2023-03-27 23:22:31 +0900

    add: #1 stream method

--- src/chapter01/Chapter010.java
+++ src/chapter01/Chapter010.java
@@ -1,5 +1,8 @@
 package chapter01;
 
+import java.util.Date;
+import java.util.stream.IntStream;
+
 /**
  * @WIRTE ellie.song
  * 모든 순열 생성 ==  재귀
@@ -7,7 +10,8 @@ package chapter01;
 public class Chapter010 {
 
     public static void permuteAndPrint(String str) {
-        permuteAndPrint("", str);
+        // permuteAndPrint("", str);
+        permuteAndPrintStream("", str);
     }
 
     private static void permuteAndPrint(String prefix, String str) {
@@ -24,7 +28,21 @@ public class Chapter010 {
         }
     }
 
+
+    // parallel => pipeline을 병렬로 처리하라고 하는 정보를 제공해주는 메소드
+    private static void permuteAndPrintStream(String prefix, String str) {
+        int n = str.length();
+        if(n == 0) {
+            System.out.println(prefix + " ");
+        } else {
+            IntStream.range(0, n)
+                    .parallel()
+                    .forEach(i -> permuteAndPrintStream(prefix + str.charAt(i), str.substring(i + 1 , n) + str.substring(0, i)));
+        }
+    }
+
     public static void main(String[] args) {
+        //Chapter010.permuteAndPrint("ABC");
         Chapter010.permuteAndPrint("ABC");
     }
 }
